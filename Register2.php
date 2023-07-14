4cda9c5298f35ce0bcbfa8a9d371800124f013dd
U1skr <137383766+U1skr@users.noreply.github.com>
2023-07-14 15:49:27 +0530

    Update Register2.php

--- Register2.php
+++ Register2.php
@@ -1,5 +1,5 @@
 <?php 
-$uname=$_POST['UNAME'];
+$uname=$_POST['uname'];
 $email1=$_POST['email1'];
 $upswd1=$_POST['upswd1'];
 $upswd2=$_POST['upswd2'];
@@ -7,11 +7,11 @@ if (!empty($uname))||(!empty($email))||(!empty($upswd1))||(!empty($upswd2))
 { $host="localhost";
 $dbusername="root";
 $dppassword=" ";
-$dbname="rephel";
+$dbname="b341";
 
 $conn=new mysqli($host,$dbusername,$dbpassword,$dbname);
 if(musqli_connect_error())
-{die('connect Error('.mysqli_connect_errno().').mysqli_connect_error());}
+{die('connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());}
 else{
 $SELECT="SELECT email From register where email=? Limit 1";
 $INSERT="INSERT into register(uname1,email,upswd1,upswd2)values(?,?,?,?)";
@@ -27,7 +27,7 @@ if($rnum==0)
 {
 $stmt->close();
 $stmt=$conn->prepare($INSERT);
-$stmt->bind_param("SSSS",$uname1,$email,$upswd1,$upswd2);
+$stmt->bind_param("ssss",$uname1,$email,$upswd1,$upswd2);
 $stmt->execute();
 echo" New record inserted Successfully";
 }
